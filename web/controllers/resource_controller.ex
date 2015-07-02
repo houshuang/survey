6fe09f72e2884613f465047e0e53e89e3b06eec2
Stian Håklev <shaklev@gmail.com>
2015-07-02 16:37:28 +0800

    Adding grade to resource_review

--- web/controllers/resource_controller.ex
+++ web/controllers/resource_controller.ex
@@ -74,6 +74,7 @@ defmodule Survey.ResourceController do
     if already > 0 do
       conn = put_flash(conn, :info, 
         "Thank you for reviewing #{already} #{resource_word(already)}. You are welcome to review more resources, or move on to other parts of the course.")
+      Survey.Grade.submit_grade(conn, "review_resource", 1.0)
     end
     
     if params["id"] do
@@ -192,6 +193,7 @@ defmodule Survey.ResourceController do
     else
       "/resource/review"
     end
+    Survey.Grade.submit_grade(conn, "review_resource", 1.0)
     
     conn
     |> ParamSession.redirect redir_url
