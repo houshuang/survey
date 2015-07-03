d58bda580dcfa3d18a146abd24412d6d7aca8269
Stian Håklev <shaklev@gmail.com>
2015-07-03 10:31:42 +0800

    Only submit grade in resource_review header if direct access

--- web/controllers/resource_controller.ex
+++ web/controllers/resource_controller.ex
@@ -74,7 +74,9 @@ defmodule Survey.ResourceController do
     if already > 0 do
       conn = put_flash(conn, :info, 
         "Thank you for reviewing #{already} #{resource_word(already)}. You are welcome to review more resources, or move on to other parts of the course.")
-      Survey.Grade.submit_grade(conn, "review_resource", 1.0)
+      if get_session(conn, :lti_grade, cache) do
+        Survey.Grade.submit_grade(conn, "review_resource", 1.0)
+      end
     end
     
     if params["id"] do
