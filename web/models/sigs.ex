e82e8a8b1f8a915a60b462bc3157910f19b34a60
Stian Håklev <shaklev@gmail.com>
2015-07-07 00:03:52 +0800

    Adding map of id to name

--- web/models/sigs.ex
+++ web/models/sigs.ex
@@ -2,6 +2,8 @@ defmodule Survey.SIG do
   use Survey.Web, :model
   alias Survey.SIG
   alias Survey.Repo
+  import Ecto.Query
+  require Ecto.Query
 
   schema "sigs" do
     field :name, :string
@@ -10,5 +12,13 @@ defmodule Survey.SIG do
 
   def name(id) do
     Survey.Repo.get(Survey.SIG, id).name
+  end
+
+  def map do
+    (from t in SIG,
+    select: [t.id, t.name])
+    |> Repo.all
+    |> Enum.map(fn [k,v] -> {k, v} end)
+    |> Enum.into(%{})
   end
 end
