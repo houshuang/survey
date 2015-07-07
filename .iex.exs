f3d5606a6344adf5100ce5ca9238e5c92f14a8cb
Stian Håklev <shaklev@gmail.com>
2015-07-07 13:27:44 +0800

    Adding reload to iex

---
+++ .iex.exs
@@ -0,0 +1,8 @@
+defmodule R do
+  def r do
+    Mix.Task.reenable "compile.elixir"
+    Application.stop(Mix.Project.config[:app])
+    Mix.Task.run "compile.elixir"
+    Application.start(Mix.Project.config[:app], :permanent)
+  end
+end
