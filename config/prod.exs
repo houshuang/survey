1bc59915ca49f35d50126682c52b9a3a715af4c0
Stian Håklev <shaklev@gmail.com>
2015-07-11 00:55:11 +0800

    Don't complain about manifest anymore

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :survey, Survey.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com"],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: "example.com"]
 
 # ## SSL Support
 #
