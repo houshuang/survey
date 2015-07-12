31ce56f8a84c7e667f42d2c48f4a3d519920d743
Stian Håklev <shaklev@gmail.com>
2015-07-12 15:33:46 +0800

    Adding question_def to Survey

--- lib/html/survey.ex
+++ lib/html/survey.ex
@@ -184,6 +184,17 @@ defmodule Survey.HTML.Survey do
       end
     end)
   end
+  
+  def question_def(struct) do
+    Enum.reduce(struct, %{}, fn x, acc -> 
+      if is_map(x) and x[:number] do
+        Map.put acc, x.number, %{type: x.type, name: x.name}
+      else
+        acc
+      end
+    end)
+  end
+
   #--------------------------------------------------------------------------------
   
   def remove_blank_lines(x) do
