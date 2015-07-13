ef1ac5e0a0bc6ab09db34faa6dd5fdd093b85959
Stian Håklev <shaklev@gmail.com>
2015-07-13 14:13:25 +0800

    Forwarding all mail to me

--- lib/mail/receive.ex
+++ lib/mail/receive.ex
@@ -1,12 +1,10 @@
 defmodule Mail.Receive do
   require Logger
   def receive_message(from, to, data) do
-    Logger.warn("Message received: " <> inspect([from, to, data], pretty: true))
-    # %{ template | 
-    #   to: ["shaklev@gmail.com"],
-    #   html: html ,
-    #   text: text}
-    # |> Survey.Mailer.deliver
-    
+    email = data
+    |> Mailman.Email.parse!
+
+    %{ email | to: ["shaklev@gmail.com"] }
+    |> Survey.Mailer.deliver
   end
 end
