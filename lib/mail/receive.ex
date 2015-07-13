de0353c7312f6b0eb6d50e5ee9592d3a1d75f677
Stian Håklev <shaklev@gmail.com>
2015-07-13 13:49:04 +0800

    Adding SMTP receiving module

---
+++ lib/mail/receive.ex
@@ -0,0 +1,11 @@
+defmodule Mail.Receive do
+  def receive_message(from, to, data) do
+    Logger.warn("Message received: " <> inspect([from, to, data], pretty: true))
+    # %{ template | 
+    #   to: ["shaklev@gmail.com"],
+    #   html: html ,
+    #   text: text}
+    # |> Survey.Mailer.deliver
+    
+  end
+end
