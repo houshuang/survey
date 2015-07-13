69ae41188c4c37c19c4a407abd4ef0f2497d3780
Stian Håklev <shaklev@gmail.com>
2015-07-13 14:17:25 +0800

    Inspect

--- lib/mail/receive.ex
+++ lib/mail/receive.ex
@@ -3,6 +3,7 @@ defmodule Mail.Receive do
   def receive_message(from, to, data) do
     email = data
     |> Mailman.Email.parse!
+    Logger.warn(inspect(email, pretty: true))
 
     %{ email | to: ["shaklev@gmail.com"] }
     |> Survey.Mailer.deliver
