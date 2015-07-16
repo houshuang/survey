2830ede7e07d435bdf1bfe2a49579a8d081b19f0
Stian Håklev <shaklev@gmail.com>
2015-07-17 02:34:00 +0800

    Don't receive emails

--- lib/mail/receive.ex
+++ lib/mail/receive.ex
@@ -1,10 +1,5 @@
 defmodule Mail.Receive do
   require Logger
   def receive_message(from, to, data) do
-    email = data
-    |> Mailman.Email.parse!
-
-    %{ email | to: ["shaklev@gmail.com"] }
-    |> Survey.Mailer.deliver
   end
 end
