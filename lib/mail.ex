ba5474c394edf6004b6f5b684e40e24dd48e34db
Stian Håklev <shaklev@gmail.com>
2015-07-21 07:30:06 +0800

    Email: How to use collab workbench

--- lib/mail.ex
+++ lib/mail.ex
@@ -10,7 +10,7 @@ defmodule Mail do
   @wk2 File.read!("data/templates/wk2.txt")
 
   def send_group_email(design_id, sender, sender_nick, subject, content, from_web \\ true) do
-    Survey.Email.insert(design_id, sender, subject, content, from_web) 
+    Survey.Email.insert(design_id, sender, subject, content, from_web)
 
     emails = Survey.DesignGroup.get_emails(design_id)
     if !from_web do
@@ -20,7 +20,7 @@ defmodule Mail do
 
     emails
     |> Enum.each(fn [id, email] ->
-      from = group_address(id, design_id) <> 
+      from = group_address(id, design_id) <>
         "-design_group@mooc.encorelab.org"
       html = Templates.group_email(content, sender_nick, id, @basename)
       %Mailman.Email{from: from, to: [email],
@@ -36,7 +36,7 @@ defmodule Mail do
   def group_address(sender, design_id) do
     hash = Hashids.encode(@hashid, [sender, design_id])
   end
-  
+
   def send_design do
     (from f in Survey.User,
     where: not is_nil(f.design_group_id))
@@ -46,9 +46,10 @@ defmodule Mail do
 
   def send_design1(user) do
     if !Survey.User.is_unsubscribed?(user.id, "collab") do
-      html = Templates.collab_wk2(user.id, @basename)
+      html = Templates.how_use(user.id, @basename)
+      text = Templates.how_use_txt(user.id, @basename)
       email = %Mailman.Email{from: "noreply@mooc.encorelab.org", to: [user.edx_email],
-        subject: "Design Strand week 2 is open!", html: html}
+        subject: "How to use the Collaborative Workbench", html: html, text: text}
       Survey.Job.add({Survey.Mailer, :deliver, [email]})
     end
   end
@@ -58,7 +59,7 @@ defmodule Mail do
       Logger.warn("Emailing disabled")
     else
       Task.Supervisor.start_child(:email_sup, fn ->
-        Enum.each(userids, fn [id, nick] -> 
+        Enum.each(userids, fn [id, nick] ->
           if !Survey.User.is_unsubscribed?(id, "collab") &&
             Survey.ChatPresence.not_online?(room, id) do
               generate_notification(entered, design, [id, nick])
@@ -75,10 +76,10 @@ defmodule Mail do
     email = (from f in Survey.User,
     where: f.id == ^id,
     select: f.edx_email) |> Survey.Repo.one
-    
-    text = Templates.collab_notification_text(nick, entered, design, 
+
+    text = Templates.collab_notification_text(nick, entered, design,
       id, @basename)
-    html = Templates.collab_notification_html(nick, entered, design, 
+    html = Templates.collab_notification_html(nick, entered, design,
       id, @basename)
 
     %Mailman.Email{
@@ -88,16 +89,14 @@ defmodule Mail do
       text: text,
       html: html }
   end
-  
+
   def send_wk2 do
     template = %Mailman.Email{
       subject: "Welcome to Week 2",
       from: "noreply@mooc.encorelab.org",
     }
 
-    # Enum.map((from f in Survey.User, where: f.id == 647) |> Survey.Repo.all, fn x -> 
-
-    Enum.map((from Survey.User) |> Survey.Repo.all, fn x -> 
+    Enum.map((from Survey.User) |> Survey.Repo.all, fn x ->
       Task.Supervisor.start_child(:email_sup, fn ->
         :timer.sleep(:random.uniform(100000))
         send_wk2_individ(template, x)
@@ -110,7 +109,7 @@ defmodule Mail do
     |> Mail.Contents.generate
     html = Templates.common(subj, text)
 
-    %{ template | 
+    %{ template |
       to: [user.edx_email],
       html: html,
       text: text}
--- lib/mail/templates.ex
+++ lib/mail/templates.ex
@@ -3,10 +3,12 @@ defmodule Mail.Templates do
   EEx.function_from_file :def, :common, "data/mailtemplates/common.eex", [:title, :content]
   EEx.function_from_file :def, :collab_notification_html, "data/mailtemplates/collab_notification.html.eex", [:name, :entered_name, :design_name, :id, :basename]
   EEx.function_from_file :def, :collab_notification_text, "data/mailtemplates/collab_notification.txt.eex", [:name, :entered_name, :design_name, :id, :basename]
-  EEx.function_from_file :def, :notification_wk1, "data/mailtemplates/to_design_wk1.html.eex", 
+  EEx.function_from_file :def, :notification_wk1, "data/mailtemplates/to_design_wk1.html.eex",
   [:cookie, :basename]
-  EEx.function_from_file :def, :collab_wk2, "data/mailtemplates/collab_wk2.html.eex", 
+  EEx.function_from_file :def, :collab_wk2, "data/mailtemplates/collab_wk2.html.eex",
   [:id, :basename]
-  EEx.function_from_file :def, :group_email, "data/mailtemplates/group_email.html.eex", 
+  EEx.function_from_file :def, :group_email, "data/mailtemplates/group_email.html.eex",
   [:content, :nick, :id, :basename]
+  EEx.function_from_file :def, :how_use, "data/mailtemplates/wiki_how_use.html.eex", [:id, :basename]
+  EEx.function_from_file :def, :how_use_txt, "data/mailtemplates/wiki_how_use.txt.eex", [:id, :basename]
 end
