31c9c6eef2ab2d6b5105e0e6bb91d754fd6c86d6
Stian Håklev <shaklev@gmail.com>
2015-07-27 21:39:00 +0800

    Formatting Brainstorm

--- lib/brainstorm.ex
+++ lib/brainstorm.ex
@@ -17,18 +17,25 @@ defmodule Brainstorm do
         nick = Survey.User.get_nick(user_id)
         state = Map.put(state, idea_id,
           %{ idea: idea, user_id: user_id, user_nick: nick, comments: [], score: 0 })
-        userstate = Map.update(userstate, user_id, [idea_id], fn user -> [ idea_id | user ] end)
+        userstate = Map.update(userstate, user_id, [idea_id], fn user ->
+          [ idea_id | user ]
+        end)
 
       {:new_comment, idea_id, user_id, comment} ->
         nick = Survey.User.get_nick(user_id)
         state = Map.update(state, idea_id, %{}, fn idea ->
-          %{ idea | comments: [ %{user_id: user_id, user_nick: nick, comment: comment} | idea.comments ] }
+          newcomment = %{user_id: user_id, user_nick: nick, comment: comment}
+          %{ idea | comments: [ newcomment | idea.comments ] }
         end)
 
       {:vote, idea_id, user_id} ->
         if not idea_id in Map.get(userstate, user_id, []) do
-          state = Map.update(state, idea_id, %{}, fn idea -> %{ idea | score: idea.score + 1 } end)
-          userstate = Map.update(userstate, user_id, [idea_id], fn user -> [ idea_id | user ] end)
+          state = Map.update(state, idea_id, %{}, fn idea ->
+            %{ idea | score: idea.score + 1 }
+          end)
+          userstate = Map.update(userstate, user_id, [idea_id], fn user ->
+            [ idea_id | user ]
+          end)
         end
     end
 
